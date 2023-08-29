032a8a6e5b58da48d8a023eea0bbce99595665f4
Kaveesh Heshan <41037571+kaveeshHeshan@users.noreply.github.com>
2023-08-29 22:13:07 +0530

    Added - Change

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Home extends Component
 {
+    #[Layout('components.layouts.child')]
     public function render()
     {
         return view('livewire.home');
